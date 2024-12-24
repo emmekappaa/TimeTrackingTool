e6815440b6f5bbe2a78fcbe15488b006b1983da9
emmekappaa <michele.cipriani@studenti.univr.it>
2024-12-24 10:30:09 +0100

    first commit

---
+++ src/main/java/demo/AppController.java
@@ -0,0 +1,16 @@
+package demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class AppController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "list";
+    }
+
+
+}
