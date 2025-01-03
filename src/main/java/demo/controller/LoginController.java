addee5d8933cfe59f4140887241765f62bb28fd0
emmekappaa <michele.cipriani@studenti.univr.it>
2025-01-03 15:42:23 +0100

    add psw control and display an error message

--- src/main/java/demo/controller/LoginController.java
+++ src/main/java/demo/controller/LoginController.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -22,32 +23,24 @@ public class LoginController {
     }
 
     @RequestMapping(value = "/login")
-    public String login(@RequestParam("username") String username, @RequestParam("password") String password, HttpSession session) {
+    public String login(@RequestParam("username") String username, @RequestParam("password") String password, HttpSession session, Model model) {
 
         Person person = repository.findByUsername(username);
-        System.out.println(username);
-        System.out.println(password);
-        System.out.println(person.getFirstName());
 
-        if(person.getPassword().equals(password)){
-
-            session.setAttribute("loggedInUser",person);
-
-            if(person instanceof Researcher)
-            {
-                //chiamo controller home Researcher
-                session.setAttribute("role", "Researcher");
-                return "redirect:/homeResearcher";
-            }
-            else
-            {
-                //chiamo controller home Manager
-                session.setAttribute("role", "Manager");
-                return "redirect:/homeManager";
-            }
-
-
+        if (person == null || !person.getPassword().equals(password)) {
+            model.addAttribute("errorMessage", "Invalid username or password.");
+            return "login";
         }
-        return "login";
+
+        session.setAttribute("loggedInUser", person);
+
+        if (person instanceof Researcher) {
+            session.setAttribute("role", "Researcher");
+            return "redirect:/homeResearcher";
+        } else {
+            session.setAttribute("role", "Manager");
+            return "redirect:/homeManager";
+        }
+
     }
 }
